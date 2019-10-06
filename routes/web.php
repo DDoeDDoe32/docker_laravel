671521d0f60497be24f73b4acee65a64d00bc8b2
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-06 15:24:07 +0900

    사용자 인증

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::resource('articles', 'ArticlesController');
 Route::get('auth/login', function() {
 	$credentials = [
 		'email' => 'john@example.com',
-		'password' => 'password'
+		'password' => 'secret'
 	];
 
 	if (! auth()->attempt($credentials)) {
@@ -29,7 +29,7 @@ Route::get('auth/login', function() {
 });
 
 Route::get('protected', function() {
-	# dump(session()->all());
+	dump(session()->all());
 
 	if (! auth()->check()) {
 		return '누구세요?';
