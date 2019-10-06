c4e56ed4e4e31b37415e874125bd5f427ed49d60
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-06 18:11:32 +0900

    엘로퀀트 orm

--- database/migrations/2019_10_06_090108_create_article_tag_table.php
+++ database/migrations/2019_10_06_090108_create_article_tag_table.php
@@ -18,7 +18,7 @@ class CreateArticleTagTable extends Migration
             $table->integer('article_id')->unsigned();
             $table->integer('tag_id')->unsigned();
 
-            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            #$table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             
         });
