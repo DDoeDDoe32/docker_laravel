a910bdb1a52d4fef72d2e060509ffe1cbe45a94c
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-07 00:07:44 +0900

    데이터베이스 시딩

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -9,8 +9,15 @@ class ArticlesTableSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        //
+        $users = App\User::all();
+
+        $users->each(function ($user) {
+        	$user->articles()->save(
+        		factory(App\Article::class)->make()
+        	);
+        });
     }
 }
