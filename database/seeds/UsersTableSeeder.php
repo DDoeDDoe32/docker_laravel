6e15fd0206c9e9e83932b1852957a97a5121c7a5
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-06 23:46:23 +0900

    데이터베이스 시딩

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-        	'name' => sprintf('%s %s', Str::random(3), Str::random(4)),
-        	'email' => Str::random(10) . '@example.com',
-        	'password' => bcrypt('password'),
-        ]);
+        factory(App\User::class, 5)->createw();
     }
 }
