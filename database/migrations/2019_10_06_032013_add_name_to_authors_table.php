762c7ae284e2f821ec0b50b306ab3743d38a683a
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-06 12:22:04 +0900

    데이터베이스 마이그레이션

--- database/migrations/2019_10_06_032013_add_name_to_authors_table.php
+++ database/migrations/2019_10_06_032013_add_name_to_authors_table.php
@@ -14,7 +14,7 @@ class AddNameToAuthorsTable extends Migration
     public function up()
     {
         Schema::table('authors', function (Blueprint $table) {
-            //
+            $table->string('name')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddNameToAuthorsTable extends Migration
     public function down()
     {
         Schema::table('authors', function (Blueprint $table) {
-            //
+            $table->dropColumn('name');
         });
     }
 }
