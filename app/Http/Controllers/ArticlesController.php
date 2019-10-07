4a4e15cd5041f285507d2ae9c01751e577f1e5c3
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-07 22:23:24 +0900

    예외처리와 디버깅

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -25,7 +25,7 @@ class ArticlesController extends Controller
     {
         $article = \App\Article::findOrFail($id);
 
-        return $ article->toArray();
+        return $article->toArray();
     	# return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.:' . $id;
     }
 
