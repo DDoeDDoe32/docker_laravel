4e97108ab2f365ed8710929a36760c837f95e44e
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-07 22:17:08 +0900

    예외처리와 디버깅

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -23,6 +23,7 @@ class ArticlesController extends Controller
 
     public function show($id)
     {
+        echo $foo;
     	return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.:' . $id;
     }
 
