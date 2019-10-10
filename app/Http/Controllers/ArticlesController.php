6861af19d43af459bb0a926773bc7b744e3b043e
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-10 22:48:14 +0900

    즉시 로드와 페이징

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,10 +8,9 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = \App\Article::latest()->paginate(3);
-        dd(view('articles.index', compact('articles'))->render());
+        $articles = \App\Article::get();
+
         return view('articles.index', compact('articles'));
-    	#return __METHOD__ . '은(는) Article 컬렉션을 조회합니다.';
     }
 
     public function create()
