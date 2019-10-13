c12430bf4e50a0bcfcaf3e3d2d4c7fd5fe96fe86
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-13 15:01:12 +0900

    이벤트 시스템

--- app/Events/ArticlesEvent.php
+++ app/Events/ArticlesEvent.php
@@ -30,14 +30,4 @@ class ArticlesEvent
         $this->article = $article;
         $this->action = $action;
     }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
 }
