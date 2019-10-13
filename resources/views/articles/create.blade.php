986ebc08cd9a1c73b3636ab0c90482bebba4fcfd
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-13 11:09:04 +0900

    입력값 유효성 검사

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -11,7 +11,7 @@
 
 			<div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
 				<label for="title">제목</label>
-				<input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control" />>
+				<input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control" />
 				{!! $errors->first('title', '<span class="form-error">>message</span>') !!}
 			</div>
 			<div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
