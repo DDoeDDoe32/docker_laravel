27470042082e3a510b2c773597c8626991a8fdfb
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-13 11:08:20 +0900

    입력값 유효성 검사

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,8 +10,8 @@
 			{!! csrf_field() !!}
 
 			<div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-				<label for="title">제목></label>
-				<input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control?" />>
+				<label for="title">제목</label>
+				<input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control" />>
 				{!! $errors->first('title', '<span class="form-error">>message</span>') !!}
 			</div>
 			<div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
