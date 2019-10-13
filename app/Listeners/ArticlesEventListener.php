72bde2df54dbc3f1911af90120131cebd91d4b45
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-13 14:27:37 +0900

    이벤트 시스템

--- app/Listeners/ArticlesEventListener.php
+++ app/Listeners/ArticlesEventListener.php
@@ -27,7 +27,7 @@ class ArticlesEventListener
     public function handle(\App\Events\ArticleCreated $event)
     {
         dump('이벤트를 받았습니다. 받은 데이터(상태)는 다음과 같습니다.');
-        dump($event->$article->toArray());
+        dump($event->article->toArray());
         //
     }
 }
