d0994b6d05cb3b3d27ccee80a5c596308add5aa2
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-16 21:50:30 +0900

    컨트롤러와 도우미 함수

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class DocsController extends Controller
 {
-    //
+    protected $docs;
+
+    public function__construct(\App\Documentation $docs)
+    {
+    	$this->docs = $docs;
+    }
+
+    public function show($file = null)
+    {
+    	$index = markdown($this->docs->get());
+    	$content = markdown($this->docs->get($file ?: 'installation.md'));
+
+    	return view('docs.show', compact('index', 'content'));
+    }
 }
