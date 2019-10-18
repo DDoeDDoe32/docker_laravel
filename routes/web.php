d6320788bf39bbafae1271fc59bc9b15e90b3bfc
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-18 21:26:36 +0900

    다듬질

--- routes/web.php
+++ routes/web.php
@@ -104,4 +104,4 @@ Route::get('docs/{file?}', function ($file = null) {
 
 Route::get('docs/{file?}', 'DocsController@show');
 
-Route::get('docs/images/{image}', 'DocsController@image');
+Route::get('docs/images/{image}', 'DocsController@image');->where('image', '[\pL-pN\._-]+-img-[0-9]{2}.png')
