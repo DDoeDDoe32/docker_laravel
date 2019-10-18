0d26d9f4bf4967096f500716919fb083a1adceec
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-18 21:54:52 +0900

    다듬질

--- routes/web.php
+++ routes/web.php
@@ -102,6 +102,7 @@ Route::get('docs/{file?}', function ($file = null) {
 });
 */
 
+/* Markdown Viewer */
 Route::get('docs/{file?}', 'DocsController@show');
-
-Route::get('docs/images/{image}', 'DocsController@image')->where('image', '[\pL-pN\._-]+-img-[0-9]{2}.png');
+Route::get('docs/images/{image}', 'DocsController@image')
+    ->where('image', '[\pL-\pN\._-]+-img-[0-9]{2}.png');
