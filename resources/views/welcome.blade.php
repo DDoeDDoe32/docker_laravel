4f56e12595a8704c0a2d633b0f41104dde81da38
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-20 18:53:57 +0900

    개발 계획과 준비

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,4 @@
-@extends('layouts.master')
-
-@section('style')
-	<style>
-		body {background: green; color: white;}
-	</style>
-@endsection
-
-@section('content')
-	<p>저는 자식 뷰의 'content' 섹션입니다.</p>
-	@include('layouts.partials.footer')
-@endsection
-
-@section('script')
-	<script>
-		alert("저는 자식 뷰의 'script' 섹션입니다.");
-	</script>
-@endsection
+<div class="container">
+	@include('flash::message')
+	@yield('content')
+</div>
