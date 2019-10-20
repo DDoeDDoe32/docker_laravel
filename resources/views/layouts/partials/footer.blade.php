3447dd4b0c9ce927e69a3a5bba873b803f431ed2
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-20 11:55:14 +0900

    개발 계획과 준비

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,15 +3,6 @@
     <li>
       <i class="fa fa-language"></i>
     </li>
-    /*
-    @foreach (config('project.locales') as $locale => $language)
-      <li {!! ($locale == $currentLocale ) ? 'class="active"' : '' !!}>
-        <a href="{{ route('locale', ['locale' => $locale, 'return' => urlencode($currentUrl)]) }}">
-          {{ $language }}
-        </a>
-      </li>
-    @endforeach
-    */
   </ul>
 
   <div>
