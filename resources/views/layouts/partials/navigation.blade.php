bf8c13e03dff7bc25399b03a0a36396de94493cd
ddoeddoe32 <ddoeddoe32@naver.com>
2019-10-20 17:24:45 +0900

    개발 계획과 준비

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -11,7 +11,7 @@
       </button>
 
       <!-- Branding Image -->
-      <a class="navbar-brand" href="{{ auth()->check() ? route('home') : route('root') }}">
+      <a class="navbar-brand" href="{{ url('/') }}">
         {{ config('app.name', 'Laravel') }}
       </a>
     </div>
@@ -19,32 +19,15 @@
     <div class="collapse navbar-collapse" id="app-navbar-collapse">
       <!-- Left Side Of Navbar -->
       <ul class="nav navbar-nav">
-        &nbsp;<li {!! str_contains(request()->path(), 'docs') ? 'class="active"' : '' !!}>
-          <a href="{{ url('docs') }}">
-            {{ trans('docs.title') }}
-          </a>
-        </li>
-        &nbsp;<li {!! str_contains(request()->path(), ['tags', 'articles']) ? 'class="active"' : '' !!}>
-           <a href="{{ route('articles.index') }}">
-             {{ trans('forum.title') }}
-           </a>
-        </li>
+        &nbsp;
       </ul>
 
       <!-- Right Side Of Navbar -->
       <ul class="nav navbar-nav navbar-right">
         <!-- Authentication Links -->
         @if (Auth::guest())
-          <li>
-            <a href="{{ route('sessions.create', ['return' => urlencode($currentUrl)]) }}">
-              {{ trans('auth.sessions.title') }}
-            </a>
-          </li>
-          <li>
-            <a href="{{ route('users.create', ['return' => urlencode($currentUrl)]) }}">
-              {{ trans('auth.users.title') }}
-            </a>
-          </li>
+          <li><a href="{{ url('/login') }}">Login</a></li>
+          <li><a href="{{ url('/register') }}">Register</a></li>
         @else
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -53,9 +36,15 @@
 
             <ul class="dropdown-menu" role="menu">
               <li>
-                <a href="{{ route('sessions.destroy') }}">
-                  {{ trans('auth.sessions.destroy') }}
+                <a href="{{ url('/logout') }}"
+                   onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                  Logout
                 </a>
+
+                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
               </li>
             </ul>
           </li>
@@ -63,4 +52,4 @@
       </ul>
     </div>
   </div>
-</nav>
+</nav> 
